1a2ca0db2aed1ded40d928d4f24f584501eb0241
pyPlacca <dhegreatprosperity10@gmail.com>
2020-06-05 21:40:23 +0000

    adds functionality
    makes the calculator usable

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import KeyGroup from  './components';
 const groups = [
 	{
 		master: 'digits',
-		sub: 'AC Del % 7 8 9 4 5 6 1 2 3 0 .'
+		sub: [
+			'AC clear', 'Del del', '% percent', 
+			'7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'
+		]
 	},
 	{
 		master: 'operators',
-		sub: '/ * - + ='
+		sub: ['/', '*', '-', '+', '= equals']
 	}
 ]
 
@@ -18,13 +21,15 @@ function App() {
   return (
     <div className='App'>
     
-      <div id='display'></div>
+      <div id='display'>
+        <h1 contenteditable="true">0</h1>
+      </div>
 
       <main>
         {
         	groups.map(
         		({master, sub}, i) => 
-        		<KeyGroup cls={master} children={sub.split(' ')} key={i} />
+        		<KeyGroup cls={master} children={sub} key={i} />
         	)
         }
       </main>
