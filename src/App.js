4ce916bdf02498568526a351b6f195d9d0619417
pyPlacca <dhegreatprosperity10@gmail.com>
2020-06-05 19:27:54 +0000

    refactors code
    makes code DRY and clean

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Digits, Operators } from  './components'
+import KeyGroup from  './components';
 
+
+const groups = [
+	{
+		master: 'digits',
+		sub: 'AC Del % 7 8 9 4 5 6 1 2 3 0 .'
+	},
+	{
+		master: 'operators',
+		sub: '/ * - + ='
+	}
+]
 
 function App() {
   return (
@@ -10,8 +21,12 @@ function App() {
       <div id='display'></div>
 
       <main>
-        <Digits />
-        <Operators />
+        {
+        	groups.map(
+        		({master, sub}, i) => 
+        		<KeyGroup cls={master} children={sub.split(' ')} key={i} />
+        	)
+        }
       </main>
 
     </div>
--- src/components.js
+++ src/components.js
@@ -1,43 +1,18 @@
 import React from 'react'
 
-class Digits extends React.Component {
-	render () {
-		const children = [
-			'AC', 'Del', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'
-		]
-		
-		return (
-			<div className='digits'>
-	         	{
-		            children
-		            .map((t, i) => <KeyButton text={t} key={i} />)
-	          	}
-	        </div>
-		)		
-    }
-}
+const KeyButton = ({text}) => <p>{text}</p>
 
-class Operators extends React.Component {
-	render () {
-		const children = ['/', '*', '-', '+', '=']
-
-		return (
-			<div className='operators'>
-				{
-		            children
-		            .map((op, i) => <KeyButton text={op} key={i} />)
-	          	}
-	        </div>
-		)
-	}
-}
-
-function KeyButton (props) {
-  return ( <p>{props.text}</p> )
+function KeyGroup({cls, children}) {
+	
+	return (
+		<div className={cls}>
+         	{
+	            children
+	            .map((t, i) => <KeyButton text={t} key={i} />)
+          	}
+        </div>
+	)		
 }
 
 
-export { 
-	Digits, 
-	Operators 
-}
+export default KeyGroup;
